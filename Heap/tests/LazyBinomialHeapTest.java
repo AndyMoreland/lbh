7b6d1b9c528e8f2e37f9260a2f48da79ecac35d6
Andrew Moreland <andy@andymo.org>
2014-04-30 00:13:49 -0700

    Adding bigger test

---
+++ Heap/tests/LazyBinomialHeapTest.java
@@ -0,0 +1,28 @@
+import junit.framework.TestCase;
+
+public class LazyBinomialHeapTest extends TestCase {
+
+    public void testGetTrees() throws Exception {
+
+    }
+
+    public void testIsEmpty() throws Exception {
+
+    }
+
+    public void testEnqueue() throws Exception {
+
+    }
+
+    public void testMin() throws Exception {
+
+    }
+
+    public void testExtractMin() throws Exception {
+
+    }
+
+    public void testMeld() throws Exception {
+
+    }
+}
